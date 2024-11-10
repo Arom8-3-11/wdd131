74f7bab047979710dff8ecf2c2678ffdfa7c9955
Arom8-3-11 <rom24003@byui.edu>
2024-11-10 01:30:58 -0700

    finally!!!!!!!!!!!!!!

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,49 +1,6 @@
-// let full = document.querySelector("#full");
-// let worldfull = document.querySelector("#world-full");
-// let short = document.querySelector("#short");
-// let medium = document.querySelector("#medium");
-// let year = document.querySelector("#year");
-// let month = document.querySelector("#month");
-// let day = document.querySelector("#day");
-// let dayofweek = document.querySelector("#dayofweek");
-// let today = new Date();
+let currentYear = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
 
-// full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
-// 	"en-US",
-// 	{
-// 		dateStyle: "full"
-// 	}
-// ).format(today)}</span>`;
-// worldfull.innerHTML = `UK: <span class="highlight">${new Intl.DateTimeFormat(
-// 	"en-UK",
-// 	{
-// 		dateStyle: "full"
-// 	}
-// ).format(today)}</span>`;
-// short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat(
-// 	"en-US",
-// 	{
-// 		dateStyle: "short"
-// 	}
-// ).format(today)}</span>`;
-// medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat(
-// 	"en-US",
-// 	{
-// 		dateStyle: "medium"
-// 	}
-// ).format(today)}</span>`;
-
-// year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
-// month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
-// day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
-// dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
-
-
-// const currentYear = document.querySelector("#currentYear");
-// const currentYear = new Date();
-// YearSpan.innerText =currentYear.getfullYear();
-
-
-
-let date = new Date().getFullYear();
-document.getElementById("currentYear").innerHTML = date;
+// Get the last modified date
+let lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModified}`;
