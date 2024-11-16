ecfd77c27717b4210de3825cb832440590618343
Arom8-3-11 <rom24003@byui.edu>
2024-11-16 09:53:34 -0700

    current update

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,3 +4,12 @@ document.getElementById("currentYear").innerHTML = currentYear;
 // Get the last modified date
 let lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModified}`;
+
+const mainnav = document.querySelector('.navigation');
+const hamButton = document.querySelector('#menu');
+// const mainnav = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hamButton.classList.toggle('show');
+});
