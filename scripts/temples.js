b4ddb11a7be77f7819d9e506eb0a37dc8df9c99f
Arom8-3-11 <rom24003@byui.edu>
2024-11-16 11:04:22 -0700

    fixed button issue

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,6 +5,9 @@ document.getElementById("currentYear").innerHTML = currentYear;
 let lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModified}`;
 
+
+
+
 const mainnav = document.querySelector('.navigation');
 const hamButton = document.querySelector('#menu');
 // const mainnav = document.querySelector('.navigation');
