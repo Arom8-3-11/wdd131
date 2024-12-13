b911f4e2ca6a009cbbdb88d23ffa28762f1f7f5c
Arom8-3-11 <rom24003@byui.edu>
2024-12-12 21:47:50 -0700

    start of final

---
+++ project/scripts/finalproject.js
@@ -0,0 +1,19 @@
+let currentYear = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
+
+// Get the last modified date
+let lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModified}`;
+
+
+
+
+const mainnav = document.querySelector('.navigation');
+const hamButton = document.querySelector('#menu');
+// const mainnav = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hamButton.classList.toggle('show');
+});
+
