2e93776608ffca9764bd714c1b97c1a57db0cde4
Arom8-3-11 <rom24003@byui.edu>
2024-12-14 22:39:16 -0700

    finally fixed the hamburger!!!

--- project/scripts/finalproject.js
+++ project/scripts/finalproject.js
@@ -1,23 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Update current year
     let currentYear = new Date().getFullYear();
     document.getElementById("currentYear").textContent = currentYear;
-
-    // Update last modified date
     let lastModified = document.lastModified;
     document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
 
-    // Handle form submission
     const form = document.getElementById('recipeForm');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         window.location.href = 'submit.php';
     });
 
-    const mainnav = document.querySelector('.navigation');
-    const hamButton = document.querySelector('#menu');
+    const contactForm = document.getElementById('contact-form');
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        window.location.href = 'contact-question.html';
+    });
+});
+
+const mainnav = document.getElementById('navigation');
+const hamButton = document.getElementById('menu');
+if (mainnav && hamButton) {
     hamButton.addEventListener('click', () => {
         mainnav.classList.toggle('show');
         hamButton.classList.toggle('show');
+
     });
-});
+}
